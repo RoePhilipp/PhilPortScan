68df24086dad3238f6bf79fb93b44ba4eb02d11d
Philipp <roesel.philipp@gmail.com>
2019-06-28 22:03:29 +0200

    initial commit

---
+++ PhilPortScan/PhilPortScan/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhilPortScan
+{
+    public class Helper
+    {
+        private IPAddress ipTarget { get; set; }
+        private int startPort { get; set; }
+        private int endPort { get; set; }
+        public Helper(string ip, string portStart, string portEnd)
+        {
+            this.ipTarget = IPAddress.Parse(ip);
+            this.startPort = Convert.ToInt32(portStart);
+            this.endPort = Convert.ToInt32(portEnd);
+        }
+
+        public  void RunScan()
+        {
+            Console.Title = "PhilPortScan - Running!";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            for(int i = startPort; i <= endPort; i++)
+            {
+                Scan(i);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "PhilPortScan";
+        }
+
+        private void Scan(int port)
+        {
+            IPEndPoint ipEndP = new IPEndPoint(this.ipTarget, port);
+            try
+            {
+                TcpClient tcp = new TcpClient(ipEndP);
+                Console.WriteLine(string.Format("{0} : {1} - OPEN", this.ipTarget.ToString(), port.ToString()));
+                tcp.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ PhilPortScan/PhilPortScan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhilPortScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PhilPortScan");
+            Console.Clear();
+            Console.Title = "PhilPortScan";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Target IP:");
+            string ip = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Start Port:");
+            string sPort = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("End Port:");
+            string ePort = Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Start? [ENTER]");
+            Console.ReadKey();
+            Helper helper = new Helper(ip, sPort, ePort);
+            helper.RunScan();
+            Console.ReadKey();
+        }
+    }
+}
